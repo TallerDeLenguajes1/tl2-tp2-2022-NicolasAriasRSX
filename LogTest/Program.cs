80cce8b3107d2146a887cb4f96eef165b01e0e67
NicolasArias <ariasmec06@gmail.com>
2022-09-09 19:13:52 -0300

    Punto 2 Listo!

---
+++ LogTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Program
+{
+    static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+           Logger.Info("Hello world");
+           Logger.Debug("Hola Mundo");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
